9876fab27bbc2631f460d8b6d5175c1cf60156e4
Chris MacNaughton <chris.macnaughton@canonical.com>
2019-12-03 06:43:39 +0100

    day1.2 completed

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -29,13 +29,13 @@ mod tests {
     #[test]
     fn validate_mass_1969() {
         let input = 1969;
-        assert_eq!(calculate_fuel(input), 654);
+        assert_eq!(calculate_fuel(input), 966);
     }
 
     #[test]
     fn validate_mass_100756() {
         let input = 100756;
-        assert_eq!(calculate_fuel(input), 33583);
+        assert_eq!(calculate_fuel(input), 50346);
     }
 }
 
@@ -46,6 +46,19 @@ fn main() {
     );
 }
 
-fn calculate_fuel(input: usize) -> usize {
-    (input as f64 / 3.0).floor() as usize - 2
+fn calculate_fuel(mut input: usize) -> usize {
+    let mut total = 0;
+    while input > 0 {
+        input = calculate_unit(input);
+        total += input;
+    }
+    total
+}
+
+fn calculate_unit(input: usize) -> usize {
+    let new_base = (input as f64 / 3.0).floor() as usize;
+    match new_base.overflowing_sub(2) {
+        (parts, false) => parts,
+        (_, true) => 0,
+    }
 }
