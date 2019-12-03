7e32513c0b6e4d45fe95e5af12cbd222fdbccbfc
Chris MacNaughton <chris.macnaughton@canonical.com>
2019-12-03 08:53:03 +0100

    wrap up day 2.2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -9,10 +9,31 @@ static INPUT: [usize; 149] = [
     131, 1, 131, 5, 135, 1, 2, 135, 139, 1, 139, 13, 0, 99, 2, 0, 14, 0,
 ];
 
+fn find_output(program: &[usize], desired: usize) -> (usize, usize) {
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let mut int = Intcode::new(program.to_vec());
+            int.set(1, noun);
+            int.set(2, verb);
+            int.run();
+            if int.get(0) == desired {
+                return (noun, verb);
+            }
+        }
+    }
+
+    unreachable!()
+}
+
 fn main() {
     let mut intcode = Intcode::new(INPUT.to_vec());
     intcode.set(1, 12);
     intcode.set(2, 2);
     intcode.run();
-    println!("{}", intcode.get(0))
+    println!("{}", intcode.get(0));
+
+    println!("Day 2.2");
+    println!();
+    let (noun, verb) = find_output(&INPUT, 19690720);
+    println!("{}{}", noun, verb)
 }
